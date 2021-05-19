9d3903248e7dfaf3b45f31986bfcdb675788ae1a
aki <lala.akira@gmail.com>
2021-05-19 12:36:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Symbol Definition
+
+## Features
+
+Go to the definition by workspace symbols.
+
+## Extension Settings
+
+- `symbolDefinition.enabledLanguages`: `[]`
---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import * as vscode from 'vscode';
+import {
+  DefinitionProvider,
+  LocationLink,
+  Position,
+  SymbolInformation,
+  SymbolKind,
+  TextDocument,
+} from 'vscode';
+
+const FREQUENT_THRESHOLD = 5;
+
+const symbolDefinitionProvider: DefinitionProvider = {
+  provideDefinition: async (
+    document: TextDocument,
+    position: Position
+  ): Promise<LocationLink[] | undefined> => {
+    const range = document.getWordRangeAtPosition(position);
+    const query = document.getText(range);
+    if (query.length === 0) {
+      return;
+    }
+
+    const symbols = (await vscode.commands.executeCommand(
+      'vscode.executeWorkspaceSymbolProvider',
+      query
+    )) as SymbolInformation[];
+
+    const filteredSymbols = symbols.filter(
+      ({ kind }) => kind === SymbolKind.Method
+    );
+    if (filteredSymbols.length > FREQUENT_THRESHOLD) {
+      return;
+    }
+
+    return filteredSymbols.map(({ location: { uri, range } }) => ({
+      targetRange: range,
+      targetUri: uri,
+    }));
+  },
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  const config = vscode.workspace.getConfiguration('symbolDefinition');
+  if (config.enabledLanguages.length === 0) {
+    return;
+  }
+
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      config.enabledLanguages,
+      symbolDefinitionProvider
+    )
+  );
+}
+
+export function deactivate() {}
